b98b5ec879cd3d976fc6b2c3edb18bb85ef917d0
Jesús Monge <jesus_jaml@outlook.com>
2022-04-22 11:13:44 -0500

    Fixed compatibility with new dependecies

--- 4-app/src/App.js
+++ 4-app/src/App.js
@@ -1,28 +1,25 @@
-import { Route, Routes } from "react-router-dom";
-import StyleGuide from "./pages/StyleGuide";
-import Home from "./pages/Home";
-import AppContent from "./content/app-content";
+import { Route, Routes } from 'react-router-dom'
+import StyleGuide from './pages/StyleGuide'
+import Home from './pages/Home'
+import AppContent from './content/app-content'
 
 const ROUTES = {
-  home: "/",
-  styleguide: "/styleguide",
-};
+  home: '/',
+  styleguide: '/styleguide',
+}
 
 const App = () => {
-  const APP_CONTENT = AppContent;
-
   return (
     <div>
-      <h1>Hello World</h1>
       <Routes>
         <Route
           path={ROUTES.styleguide}
-          element={<StyleGuide content={APP_CONTENT} />}
+          element={<StyleGuide content={AppContent} />}
         />
-        <Route path={ROUTES.home} element={<Home content={APP_CONTENT} />} />
+        <Route path={ROUTES.home} element={<Home content={AppContent} />} />
       </Routes>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- 4-app/src/components/Nav/Nav.js
+++ 4-app/src/components/Nav/Nav.js
@@ -1,39 +1,34 @@
-import React from 'react';
-import Cta from '../Cta/Cta';
-import './Nav.scss';
+import React from 'react'
+import Cta from '../Cta/Cta'
+import './Nav.scss'
 
-function Nav(props) {
-  const content = props.content;
-  const cssClass = 'Nav';
+const Nav = ({ content, type }) => {
+  const cssClass = 'Nav'
   const uiConfig = {
     cta: 'cuaternary',
-    icon: 'secondary'
+    icon: 'secondary',
   }
 
-  const List = (() => {
-    return content.items.map((item, index) => {
-      return (
-        <li key={index} className={`${cssClass}-item`}>
-          <a href={item.url}>
-            {item.label}
-          </a>
-        </li>
-      )
-    })
-  })();
-
   return (
-    <div className={`${cssClass} ${cssClass}--${props.type}`}>
+    <div className={`${cssClass} ${cssClass}--${type}`}>
       <div className="grid">
         <div className={`${cssClass}-button`}>
-          <Cta type={uiConfig.cta} icon={content.icon} iconType={uiConfig.icon} />
+          <Cta
+            type={uiConfig.cta}
+            icon={content.icon}
+            iconType={uiConfig.icon}
+          />
         </div>
         <ul className={`${cssClass}-list`}>
-          {List}
+          {content.items.map((item, i) => (
+            <li key={i} className={`${cssClass}-item`}>
+              <a href={item.url}>{item.label}</a>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
-  );
+  )
 }
 
-export default Nav;
+export default Nav
